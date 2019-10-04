efa92890db0d38ef07807865da5797122b23241e
yc <247067345@qq.com>
2019-10-05 00:27:55 +0800

    Initial commit

---
+++ jcenterlib/src/main/java/com/example/jcenterlib/JcenterLibActivity.java
@@ -0,0 +1,14 @@
+package com.example.jcenterlib;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class JcenterLibActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jcenter_lib);
+    }
+}
